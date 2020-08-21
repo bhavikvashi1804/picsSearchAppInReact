7aee7f5e723c633b510830d261ff6e67762c48e9
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-21 10:05:02 +0530

    App structure

---
+++ src/Components/ImageList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageList=()=>{
+    return (<h1>This is image list</h1>);
+}
+
+export default ImageList;
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearchBar=()=>{
+    return (<h1>This is search bar</h1>);
+}
+
+export default SearchBar;
