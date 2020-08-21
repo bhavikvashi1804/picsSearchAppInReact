b63604aef690f7715f47684c44128ea6f77c25e0
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-21 10:52:25 +0530

    Input Element onClick and onChange

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from "react";
 
-class SearchBar extends React.Component{
+class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+  //the name of the event is as
+  //{on}{nameOfElement}{Event}
+  //some time on = handle
+  onInputClick(){
+    console.log('I got clicked');
+  }
 
-    render(){
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange} onClick={this.onInputClick}/>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
