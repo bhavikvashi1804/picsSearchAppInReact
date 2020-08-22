83d2386b0c9b9cb05028e189194cb3327c9ac8a4
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-22 13:00:11 +0530

    update the state data which is loaded from API

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,20 +5,22 @@ import ImageList from "./ImageList";
 import ACCESS_KEY from "../secure";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    var response=await axios.get("https://api.unsplash.com/search/photos", {
+  state = { images: [] };
+ onSearchSubmit=async(term)=> {
+    var response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
-        Authorization: `Client-ID ${ACCESS_KEY}`
+        Authorization: `Client-ID ${ACCESS_KEY}`,
       },
     });
-
-    console.log(response.data.results);
+    this.setState({images:response.data.results});
   }
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmitReq={this.onSearchSubmit} />
+        Found {this.state.images.length} Images
         <ImageList />
       </div>
     );
