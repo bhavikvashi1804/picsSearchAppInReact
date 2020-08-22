763239612c5b8489c47958ba2d2f2bbe2836faef
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-22 12:40:38 +0530

    Get the data

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,17 +1,23 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
+import ACCESS_KEY from "../secure";
+import SECRET_KEY from "../secure";
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log("Pass the data up");
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: `Client-ID ${ACCESS_KEY}`
+      },
+    });
   }
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmitReq={this.onSearchSubmit}/>
+        <SearchBar onSubmitReq={this.onSearchSubmit} />
         <ImageList />
       </div>
     );
