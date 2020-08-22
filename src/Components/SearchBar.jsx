15c565d3991efffecb8d41f951ae48f86582ff7e
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-22 11:45:41 +0530

    Format the document

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,20 +3,18 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit(event){
+  onFormSubmit(event) {
     event.preventDefault();
-    //the browser will not discard the value of form field if user press enter 
-    console.log("onSubmit is called");
     console.log(this.state.term);
-    //this gives you error that state is not defined
-    //to get the value use this
-    
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" onSubmit={(event)=>this.onFormSubmit(event)}>
+        <form
+          className="ui form"
+          onSubmit={(event) => this.onFormSubmit(event)}
+        >
           <div className="field">
             <label>Image Search</label>
             <input
@@ -24,7 +22,6 @@ class SearchBar extends React.Component {
               value={this.state.term}
               onChange={(event) => {
                 this.setState({ term: event.target.value.toUpperCase() });
-                //this is called controlled component
               }}
             />
           </div>
