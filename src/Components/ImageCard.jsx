154e77e42104b8d1c8ebcdd1235cddfca29ebcf1
bhavikvashi1804 <bhavikvashi1804@gmail.com>
2020-08-22 14:39:14 +0530

    get the image height dynamically

--- src/Components/ImageCard.jsx
+++ src/Components/ImageCard.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 class ImageCard extends React.Component{
 
-   
+    constructor(props){
+        super(props);
+        this.imageRef=React.createRef();
+    }
 
+    componentDidMount(){
+        this.imageRef.current.addEventListener('load',this.setSpans);
+    }
+
+    setSpans=()=>{
+        console.log(this.imageRef.current.clientHeight);
+        //this is the only way to access the height
+        //if you access the height in componentDidMount it gives you 0
+        //even if in componentDidMount when you print this.imageRef then it gives some value of client height
+    }
+    
     render(){
         const {description,urls}=this.props.image;
         return(
             <div>
-                <img src={urls.regular} alt={description}/>
+                <img src={urls.regular} alt={description} ref={this.imageRef}/>
             </div>
         );
     }
